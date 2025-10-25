54c98a3e38d97ddccef029ffc26a586b8b61cb63
cheekopanda0323-afk <cheekopanda0323@gmail.com>
2025-10-25 06:28:19 -1100

    Update players via Discord bot

--- lib/db.ts
+++ lib/db.ts
@@ -463,6 +463,37 @@ function getDefaultPlayers(): Player[] {
         points: 0
       }
     }
+  },
+  {
+    id: "17",
+    name: "itzhtg1",
+    overallPoints: 4,
+    stats: {
+      UHC: {
+        tier: "N/A",
+        points: 0
+      },
+      CRYSTAL: {
+        tier: "HT4",
+        points: 4
+      },
+      SWORD: {
+        tier: "N/A",
+        points: 0
+      },
+      NETHPOT: {
+        tier: "N/A",
+        points: 0
+      },
+      SMP: {
+        tier: "N/A",
+        points: 0
+      },
+      SMPKIT: {
+        tier: "N/A",
+        points: 0
+      }
+    }
   }
 ]
 }
