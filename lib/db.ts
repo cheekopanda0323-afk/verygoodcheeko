d7257d4c33895f14f6a31cb9b3be6efabb46c764
cheekopanda0323-afk <cheekopanda0323@gmail.com>
2025-10-30 21:40:05 -1100

    Update players via Discord bot

--- lib/db.ts
+++ lib/db.ts
@@ -649,6 +649,37 @@ function getDefaultPlayers(): Player[] {
         points: 0
       }
     }
+  },
+  {
+    id: "23",
+    name: "SLBChocos",
+    overallPoints: 2,
+    stats: {
+      UHC: {
+        tier: "N/A",
+        points: 0
+      },
+      CRYSTAL: {
+        tier: "N/A",
+        points: 0
+      },
+      SWORD: {
+        tier: "N/A",
+        points: 0
+      },
+      NETHPOT: {
+        tier: "N/A",
+        points: 0
+      },
+      SMP: {
+        tier: "N/A",
+        points: 0
+      },
+      SMPKIT: {
+        tier: "HT5",
+        points: 2
+      }
+    }
   }
 ]
 }
