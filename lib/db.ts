e5c2c53914395318230240cbe6e5dd1907d68db1
cheekopanda0323-afk <cheekopanda0323@gmail.com>
2025-12-03 15:42:23 -1100

    Update players via Discord bot

--- lib/db.ts
+++ lib/db.ts
@@ -1602,6 +1602,45 @@ function getDefaultPlayers(): Player[] {
         points: 3
       }
     }
+  },
+  {
+    id: "141",
+    name: "check",
+    overallPoints: 1,
+    stats: {
+      SMP: {
+        tier: "N/A",
+        points: 0
+      },
+      Crystal: {
+        tier: "N/A",
+        points: 0
+      },
+      "Mace PvP": {
+        tier: "N/A",
+        points: 0
+      },
+      Sword: {
+        tier: "N/A",
+        points: 0
+      },
+      DiaSMP: {
+        tier: "N/A",
+        points: 0
+      },
+      UHC: {
+        tier: "N/A",
+        points: 0
+      },
+      Nethpot: {
+        tier: "LT5",
+        points: 1
+      },
+      AxePvP: {
+        tier: "N/A",
+        points: 0
+      }
+    }
   }
 ]
 }
